11de0bc24ff61505a754679fbe4e1e0c311af21b
eclips0r <lukaswachter@gmx.net>
2020-09-10 12:57:59 +0200

    initial commit

---
+++ MedianQuickSort.java
@@ -0,0 +1,35 @@
+/** Docs folgen... **/
+
+public class MedianQuickSort extends QuickSort {
+    int[] array;
+
+    public MedianQuickSort(int[] array) {
+        this.array = array;
+    }
+
+    public int[] medianQuicksort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = medianPartition(A, p, r);
+            medianQuicksort(A, p, q - 1);
+            medianQuicksort(A, q + 1, r);
+        }
+        return A;
+    }
+
+    public int medianPartition(int[] A, int p, int r) {
+        int i = median(A, p, (int) Math.floor((p + r) / 2), r);
+        int temp = A[r];
+        A[r] = A[i];
+        A[i] = temp;
+        return partition(A, p, r);
+    }
+
+    public int median(int[] A, int i, int j, int k) {
+        if (A[i] <= A[j] && A[k] <= A[i]) {
+            return i;
+        } else if (A[i] <= A[j] && A[k] >= A[j]) {
+            return j;
+        } else
+            return k;
+    }
+}
